6b1bc49f6b502e907d8e7b83d12ce9e56a38a263
gxmh <858353442@qq.com>
2021-04-12 23:59:06 +0800

    for循环

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	/*
+	write your code here
         double a = 1.0;
         double b = 3.0;
         double c = -4.0;
@@ -18,5 +20,26 @@ public class Main {
         System.out.println(r1);
         System.out.println(r2);
         System.out.println(r1 == 1 && r2 == -4 ? "测试通过" : "测试失败");
+
+        int[] nub=new int[5];
+        String str[] ={"a","b","c"};
+        System.out.println(str.length);
+
+        Scanner sca= new Scanner(System.in);
+        System.out.println("input your name?");
+        String name = sca.nextLine();
+        System.out.println("input your name");
+        int age=sca.nextInt();
+
+        System.out.println("input TRUE or FALSE");
+        Boolean flag = sca.nextBoolean();
+        System.out.printf("Hi, %s,you are %d\n "+flag,name,age);
+
+	 */
+        String str[] ={"a","b","c"};
+        for (String n: str ) {
+            System.out.println(n);
+
+        }
     }
 }
